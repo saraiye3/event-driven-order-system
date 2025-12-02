fd83830d230c0e44de9110e5e69d3e63de132529
saraiye3 <sarai171298@gmail.com>
2025-12-02 16:01:39 +0200

    fixed seen_id set bug

--- producer/web_api.py
+++ producer/web_api.py
@@ -21,6 +21,7 @@ def register_routes(app, publisher):
 
         if order_id in seen_order_ids:
             return jsonify({"error": "orderId already exists"}), 409
+        seen_order_ids.add(order_id)
 
         # Generate order
         order = OrderGenerator.generate_order(order_id, num_of_items)
